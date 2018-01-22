db7362178f9680437f4d8530a142a2ba164bb883
okgrim <35685710+okgrim@users.noreply.github.com>
2018-01-22 22:46:09 +0100

    Add files via upload
    Lagt til innerhtml funksjon som endrer innholdet på drop-down menyen til utesteder. Har ikke fixet scriptet til google maps, men har lagt til et eget for endring av dokumentet

--- script1.js
+++ script1.js
@@ -1,4 +1,35 @@
-function navnetMitt() {
-    var x = document.getElementById('mittNavn');
-      x.innerHTML = "Skikkelig bra navn";
+function initMap() {
+  var ricks = {lat: 60.392636, lng: 5.321342};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,
+    center: ricks,
+  });
+
+  var locations = [
+  ["Ricks", 60.392636, 5.321342, 1],
+  ["Don Pippo", 60.389680, 5.322974, 2],
+  ["Garage", 60.389586, 5.323835, 3],
+];
+
+  var infowindow = new google.maps.InfoWindow();
+
+  var marker,i;
+
+  for (i = 0; i < locations.length; i++) {
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+      map: map
+    });
   }
+
+    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+      return function() {
+        infowindow.setContent(locations[i][0]);
+        infowindow.open(map, marker);
+      }
+    })(marker, i));
+
+
+
+
+}
---
+++ script2.js
@@ -0,0 +1,41 @@
+var pippoTekst =
+'<div id="innhold1">'+
+'<div id="tekst1">'+
+'<p> Don Pippo er en bar</p>'+
+'<p> som holder til like ved en skitten kebab</p>'+
+'<p> Her kan du sitte alene som illustrert på bildet</p>'+
+'</div>'+
+'<img src="pippobilde.jpg">'+
+'</div>';
+
+var garageTekst =
+'<div id="innhold2">'+
+'<div id="tekst1">'+
+'<p> Garage er et høl med gamle menn,</p>'+
+'<p> som antaster andre menn mot deres vilje.</p>'+
+'</div>'+
+'<img src="garagebilde.jpg">'+
+'</div>';
+
+var ricksTekst =
+'<div id="innhold3">'+
+'<div id="tekst1">'+
+'<p> Ricks er stedet for deg</p>'+
+'<p> som ønsker å tafse på mindreårige</p>'+
+'</div>'+
+'<img src="ricksbilde.png">'+
+'</div>';
+
+
+/*Funksjonene under endrer innholdet i <div id="pageContent">*/
+function pippoKlikk(){
+document.getElementById("pageContent").innerHTML = (pippoTekst);
+}
+
+function garageKlikk(){
+document.getElementById("pageContent").innerHTML = (garageTekst);
+}
+
+function ricksKlikk(){
+document.getElementById("pageContent").innerHTML = (ricksTekst);
+}
