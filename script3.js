33d988ed1a9d2f34e0c368f6f17c10016f2b32af
okgrim <35685710+okgrim@users.noreply.github.com>
2018-03-06 18:53:52 +0100

    Add files via upload
    Her har dere dummy-filen for hvordan googlemaps funker med toalettgreiene. Nederst finner dere starten på regex delen, men den er langt fra ferdig så se bort i fra den(Jeg blir gal av de greiene der).

---
+++ script3.js
@@ -0,0 +1,360 @@
+var toaletter = [
+  {
+  "herre":"1",
+  "tid_sondag":"07.00 - 23.15",
+  "pissoir_only":"NULL",
+  "stellerom":"NULL",
+  "latitude":"60.388076",
+  "tid_hverdag":"07.00 - 23.15",
+  "plassering":"NONNESETER TERMINAL, SØR",
+  "tid_lordag":"07.00 - 23.15",
+  "rullestol":"1",
+  "adresse":"Lungegårdskaien",
+  "pris":"12",
+  "id":"1",
+  "place":"NONNESETER TERMINAL, SOUTH",
+  "dame":"1",
+  "longitude":"5.334937"
+  },
+  {
+  "herre":"1",
+  "tid_sondag":"NULL",
+  "pissoir_only":"NULL",
+  "stellerom":"NULL",
+  "latitude":"60.388871",
+  "tid_hverdag":"05.30 - 23.50",
+  "plassering":"NONNESETER TERMINAL , NORD",
+  "tid_lordag":"07.00 - 23.15",
+  "rullestol":"1",
+  "adresse":"Østre Strømkai",
+  "pris":"12",
+  "id":"2",
+  "place":"NONNESETER TERMINAL , NORTH",
+  "dame":"1",
+  "longitude":"5.334079"
+  },
+  {
+  "herre":"1",
+  "tid_sondag":"NULL",
+  "pissoir_only":"NULL",
+  "stellerom":"NULL",
+  "latitude":"60.388868",
+  "tid_hverdag":"09.00 - 17.00",
+  "plassering":"SKYSS KUNDESENTER",
+  "tid_lordag":"09.00 - 15.00",
+  "rullestol":"1",
+  "adresse":"Østre Strømkai",
+  "pris":"12",
+  "id":"3",
+  "place":"SKYSS CUSTOMER CENTRE",
+  "dame":"1",
+  "longitude":"5.334079"
+  },
+  {
+  "herre":"1",
+  "tid_sondag":"07.00 - 23.00",
+  "pissoir_only":"NULL",
+  "stellerom":"NULL",
+  "latitude":"60.39041",
+  "tid_hverdag":"07.00 - 23.00",
+  "plassering":"JERNBANESTASJONEN",
+  "tid_lordag":"07.00 - 23.00",
+  "rullestol":"NULL",
+  "adresse":"Strømgaten 4",
+  "pris":"10",
+  "id":"4",
+  "place":"RAILWAY STATION",
+  "dame":"1",
+  "longitude":"5.332995"
+  },
+  {
+  "herre":"1",
+  "tid_sondag":"08.30 - 22.00",
+  "pissoir_only":"NULL",
+  "stellerom":"1",
+  "latitude":"60.394554",
+  "tid_hverdag":"09.00 - 23.00",
+  "plassering":"MATHALLEN",
+  "tid_lordag":"08.30 - 22.00",
+  "rullestol":"1",
+  "adresse":"Strandkaien 3",
+  "pris":"10",
+  "id":"5",
+  "place":"FISH MARKET",
+  "dame":"1",
+  "longitude":"5.324099"
+  },
+  {
+  "herre":"1",
+  "tid_sondag":"08.00 - 18.00",
+  "pissoir_only":"NULL",
+  "stellerom":"",
+  "latitude":"60.395432",
+  "tid_hverdag":"08.00 - 18.00",
+  "plassering":"STRANDKAITERMINALEN",
+  "tid_lordag":"08.00 - 18.00",
+  "rullestol":"",
+  "adresse":"Strandkaien",
+  "pris":"10",
+  "id":"6",
+  "place":"STRANDKAI BOAT TERMINAL",
+  "dame":"1",
+  "longitude":"5.321034"
+  },
+  {
+  "herre":"1",
+  "tid_sondag":"NULL",
+  "pissoir_only":"NULL",
+  "stellerom":"NULL",
+  "latitude":"60.3913929",
+  "tid_hverdag":"08.00 - 15.00",
+  "plassering":"BERGEN KOMMUNE, INNBYGGERSERVICE",
+  "tid_lordag":"NULL",
+  "rullestol":"1",
+  "adresse":"Kaigaten 4",
+  "pris":"0",
+  "id":"7",
+  "place":"CITIZEN SERVICE CENTRE",
+  "dame":"1",
+  "longitude":"5.326855"
+  },
+  {
+  "herre":"1",
+  "tid_sondag":"NULL",
+  "pissoir_only":"NULL",
+  "stellerom":"1",
+  "latitude":"60.3891794",
+  "tid_hverdag":"09.00 - 21.00",
+  "plassering":"BERGEN STORSENTER",
+  "tid_lordag":"09.00 - 18.00",
+  "rullestol":"1",
+  "adresse":"Strømgaten 8",
+  "pris":"10",
+  "id":"8",
+  "place":"BERGEN STORSENTER",
+  "dame":"1",
+  "longitude":"5.3305793"
+  },
+  {
+  "herre":"1",
+  "tid_sondag":"NULL",
+  "pissoir_only":"NULL",
+  "stellerom":"1",
+  "latitude":"60.392209",
+  "tid_hverdag":"09.00 - 21.00",
+  "plassering":"SUNDT MOTEHUS",
+  "tid_lordag":"09.00 - 18.00",
+  "rullestol":"1",
+  "adresse":"Torgallmenningen 14",
+  "pris":"10",
+  "id":"9",
+  "place":"SUNDT FASHION HOUSE",
+  "dame":"1",
+  "longitude":"5.324011"
+  },
+  {
+  "herre":"1",
+  "tid_sondag":"NULL",
+  "pissoir_only":"NULL",
+  "stellerom":"1",
+  "latitude":"60.3928444",
+  "tid_hverdag":"09.00 - 20.00",
+  "plassering":"XHIBITION",
+  "tid_lordag":"09.00 - 18.00",
+  "rullestol":"1",
+  "adresse":"Småstrandgaten 3",
+  "pris":"10",
+  "id":"10",
+  "place":"XHIBITION",
+  "dame":"1",
+  "longitude":"5.3239541"
+  },
+  {
+  "herre":"1",
+  "tid_sondag":"NULL",
+  "pissoir_only":"NULL",
+  "stellerom":"1",
+  "latitude":"60.3947371",
+  "tid_hverdag":"09.00 - 21.00",
+  "plassering":"GALLERIET",
+  "tid_lordag":"09.00 - 18.00",
+  "rullestol":"1",
+  "adresse":"Torgallmenningen 8",
+  "pris":"10",
+  "id":"11",
+  "place":"GALLERIET",
+  "dame":"1",
+  "longitude":"5.3243561"
+  },
+  {
+  "herre":"1",
+  "tid_sondag":"NULL",
+  "pissoir_only":"NULL",
+  "stellerom":"1",
+  "latitude":"60.3944194",
+  "tid_hverdag":"10.00 - 20.00",
+  "plassering":"KLØVERHUSET",
+  "tid_lordag":"10.00 - 18.00",
+  "rullestol":"1",
+  "adresse":"Strandgaten 13 -15",
+  "pris":"10",
+  "id":"12",
+  "place":"KLØVERHUSET",
+  "dame":"1",
+  "longitude":"5.3205649"
+  },
+  {
+  "herre":"1",
+  "tid_sondag":"09.00 - 18.00",
+  "pissoir_only":"NULL",
+  "stellerom":"NULL",
+  "latitude":"60.3974108",
+  "tid_hverdag":"09.00 - 18.00",
+  "plassering":"BRYGGEN BESØKSSENTER",
+  "tid_lordag":"09.00 - 18.00",
+  "rullestol":"1",
+  "adresse":"Jacobsfjorden, Bryggen",
+  "pris":"10",
+  "id":"13",
+  "place":"BRYGGEN VISITOR CENTRE",
+  "dame":"1",
+  "longitude":"5.3222145"
+  },
+  {
+  "herre":"NULL",
+  "tid_sondag":"ALL",
+  "pissoir_only":"1",
+  "stellerom":"NULL",
+  "latitude":"60.397359",
+  "tid_hverdag":"ALL",
+  "plassering":"C. SUNDTSGT",
+  "tid_lordag":"ALL",
+  "rullestol":"NULL",
+  "adresse":"C. Sundts gt",
+  "pris":"NULL",
+  "id":"14",
+  "place":"C. SUNDTSGT",
+  "dame":"NULL",
+  "longitude":"5.313963"
+  },
+  {
+  "herre":"NULL",
+  "tid_sondag":"ALL",
+  "pissoir_only":"1",
+  "stellerom":"NULL",
+  "latitude":"60.397557",
+  "tid_hverdag":"ALL",
+  "plassering":"NORDNES SKOLE",
+  "tid_lordag":"ALL",
+  "rullestol":"NULL",
+  "adresse":"Nordnesparken 3",
+  "pris":"NULL",
+  "id":"15",
+  "place":"NORDNES SKOLE</a>",
+  "dame":"NULL",
+  "longitude":"5.307858"
+  }
+];
+
+
+var markers = [];
+
+var i = 0;
+toaletter.forEach(function(toalett) {
+  i++;
+  var tjohei = document.getElementById("smør");
+  tjohei.innerHTML += "toalett " + i +" ved " + "<a id='toalettlenke' onclick='klikkern(" + toalett.id + ")'>" + toalett.place + "</button>" + ("</br>");
+});
+
+
+function initMap() {
+   var infowindow = new google.maps.InfoWindow();
+  var bergen = {lat: 60.392209, lng: 5.324011};
+  var map = new google.maps.Map(document.getElementById('googlemap'), {
+    zoom: 13,
+    center: bergen
+  });
+  infoWindow = new google.maps.InfoWindow;
+
+  //Under kommer Geolocation
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      var icon = {url:"pepe.png",
+      scaledSize: new google.maps.Size(60, 60)
+      };
+
+      marker = new google.maps.Marker({
+            position: pos,
+            map: map,
+            contentString: contentString,
+            icon: icon,
+      });
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Her står du.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
+  // Under kommer Markers og infowindow
+
+
+  for(var i = 0; i < toaletter.length; i++){
+      var latLng = new google.maps.LatLng(toaletter[i].latitude, toaletter[i].longitude);
+      var contentString = toaletter[i].id + ". " + toaletter[i].place;
+
+      marker = new google.maps.Marker({
+            position: latLng,
+            label: toaletter[i].id,
+            map: map,
+            contentString: contentString
+      });
+
+
+
+
+      marker.addListener('click', function() {
+             infowindow.setContent(this.contentString);
+             infowindow.open(map, this);
+             map.setCenter(this.getPosition());
+       });
+           markers.push(marker);
+  }
+}
+// Benytter seg av Markers arrayet for å kunne referere til spesifikke iWindows
+function klikkern(i){
+  google.maps.event.trigger(markers[i], 'click');
+}
+
+//Under kommer søkefunksjonens kode SE BORT I FRA DETTE
+/*
+function søk(){
+  var tekst = document.getElementById("search");
+  var tekstverdi = tekst.value;
+
+  var kriteriet = /mann|kvinne|dame|gutt|jente|rullestol|hc|handicap|gratis/i;
+  var resultat = kriteriet.exec(tekst);
+
+  if(resultat != null){
+    console.log(String(resultat));
+  }
+}
+*/
