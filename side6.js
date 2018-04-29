ef83520c3972cc6836a8b09f6209db205c7c0e58
harste <35685822+harste@users.noreply.github.com>
2018-04-29 16:29:15 +0200

    Add files via upload

--- side6.js
+++ side6.js
@@ -1,4 +1,6 @@
+var toalett = new XMLHttpRequest;
 var leke = new XMLHttpRequest;
+
 var url = "https://hotell.difi.no/api/json/bergen/lekeplasser?";
 leke.onreadystatechange = function(){
   if(leke.readyState === 4 && this.status === 200){
@@ -14,7 +16,23 @@ leke.onreadystatechange = function(){
 leke.open("GET", url, true);
 leke.send();
 
+var url = "https://hotell.difi.no/api/json/bergen/dokart?";
+toalett.onreadystatechange = function(){
+  if(toalett.readyState === 4 && this.status === 200){
+    var ferdig = JSON.parse(toalett.responseText);
+    var liste = ferdig.entries;
+    for(i=0; i < liste.length; i++){
+      toalettliste.push(liste[i]);
+    }
+    console.log(ferdig);
+    // toalettListeFunksjon(ferdig);
+  }
+}
+toalett.open("GET", url, true);
+toalett.send();
+
 var lekeplass = [];
+var toalettliste = [];
 
 var tull = document.getElementById("select");
 function listeFunksjon(lekeplass) {
@@ -39,7 +57,7 @@ function clickFavourite(leke) {
       kjørOption.innerHTML += "<div>" + "<p>" + "Du valgte " + alt.navn + " som din favorittlekeplass!" + "</p>" + "</br>" +
       "Litt info om denne lekeplassen:" + "</br>" + "ID: " + alt.id + "</br>" +
       "Longitude: " + alt.longitude +
-      "</br>" + "Latitude: " + alt.latitude + "</br>" + "Nærmeste toalett: " +  + "</div>";
+      "</br>" + "Latitude: " + alt.latitude + "</br>" + "</div>";
       console.log(JSON.parse(x[i].value));
     }
     // else {
@@ -61,23 +79,69 @@ function distance(lat1, lon1, lat2, lon2) {
           dist = dist * 180/Math.PI;
           dist = dist * 60 * 1.1515;
           dist = dist * 1.609344;
-      document.getElementById('gress').innerHTML = ("Avstanden er " + Math.round(dist*10)/10 + "km");
+          // document.getElementById('hemmeligDo').innerHTML = ("Avstanden er " + Math.round(dist*10)/10 + "km");
+          console.log(dist);
+          avstandsListe.push(Math.round(dist*10)/10);
 }
 
-function xVsY(){
-  var korteste;
-  var vinnern;
-//  var kjøttpølse = selected.latitude, selected.longitude;
-  toaletter.forEach(function(element){
-    var mellomkuk = distance(kjøttpølse,element.latitude, elelemnt.longitude);
-    if(korteste == undefined){
-      korteste = mellomkuk;
-      vinnern = element;
+
+var avstandsListe = [];
+
+function doDistance(leke) {
+  var nuts = document.getElementById("hemmeligDo");
+  var y = leke;
+  nuts.innerHTML = "";
+  var x = tull.options;
+  for(i=0; i<x.length; i++){
+    if(x[i].selected === true) {
+      var avstand = JSON.parse(x[i].value);
+      var opp = avstand.latitude;
+      var bort = avstand.longitude;
+      console.log(opp, bort);
+      var n = 0;
+      toalettliste.entries.forEach(function(){
+        n++;
+        var doLat = toalettliste.entries[n].latitude;
+        var doLong = toalettliste.entries[n].longitude;
+        distance(opp, bort, doLat, doLong);
+        console.log(opp, bort, doLat, doLong);
+      })
+
+      // while(n<toalettliste.entries.length) {
+      //   setInterval(function(){n++;}, 1000);
+      //   console.log(toalettliste.entries[n].latitude, toalettliste.entries[n].longitude);
+      //   distance(opp, bort, toalettliste.entries[n].latitude, toalettliste.entries[n].longitude);
+      // }
+
+      // for(n=0; n<toalettliste.entries.length; n++){
+      //   distance(opp, bort, toalettliste.entries[n].latitude, toalettliste.entries[n].longitude);
+      // }
     }
-    else if(mellomkuk < korteste){
-      korteste = mellomkuk;
-      vinnern = element;
-    }
-  })
-  console.log(vinnern);
+    // )
+  }
+  console.log(avstandsListe);
 }
+
+
+// function doDistance(leke) {
+//   var nuts = document.getElementById("hemmeligDo");
+//   var y = leke;
+//   nuts.innerHTML = "";
+//   var x = tull.options;
+//   for(i=0; i<x.length; i++){
+//     if(x[i].selected === true) {
+//       var avstand = JSON.parse(x[i].value);
+//       var bort = avstand.longitude;
+//       var opp = avstand.latitude;
+//       console.log(opp, bort);
+//       for(n=0; n<toalettliste.entries.length; n++){
+//         distance(opp, bort, toalettliste.entries[n].latitude, toalettliste.entries[n].longitude);
+//       }
+//       // toalettliste.forEach(function(){
+//       //   distance(opp, bort, toalettliste.entries.latitude, toalettliste.entries.longitude);
+//       // })
+//     }
+//     // )
+//   }
+//   console.log(avstandsListe);
+// }
